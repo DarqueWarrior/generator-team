a98d35d6aa6b7e6c1439e7b2a442db7227ccdd30
Donovan Brown <dbrown@microsoft.com>
2017-04-20 06:49:22 -0500

    Full path.

--- test/project.Tests.ps1
+++ test/project.Tests.ps1
@@ -1,7 +1,7 @@
 Describe 'Project' {
    Context 'Create' {
 
-      yo team:project unitTest demonstrations $env:PAT
+      node .\node_modules\yo\lib\cli.js team:project unitTest demonstrations $env:PAT
       
       It 'Should create project' {
          Get-Project -Name unitTest | Should not be $Null
