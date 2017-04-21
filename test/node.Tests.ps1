64d3edbf65a82b2801ef72cfe8c59b42560540fb
Donovan Brown <dbrown@microsoft.com>
2017-04-21 15:41:21 -0500

    Added docker tests for node.

--- test/node.Tests.ps1
+++ test/node.Tests.ps1
@@ -1,8 +1,9 @@
-Describe 'Node' {
+Describe 'Node_PaaS' {
+   
    $projectName = "project" + (New-Guid).Guid.SubString(0, 5)
 
-   Context 'PaaS' {
-      node $env:YO team node $projectName demonstrations PM_DonovanBrown " " " " " " Default paas false " " " " " " " " " " " " " " " " $env:PAT *> .\nodeProject.log
+   Context 'Full' {
+      node $env:YO team node $projectName demonstrations PM_DonovanBrown " " " " " " Default paas false " " " " " " " " " " " " " " " " $env:PAT *> .\nodePaaS.log
       
       It 'Should create project' {
          Get-Project | Where-Object {$_.Name -eq $projectName} | Should not be $Null
@@ -16,9 +17,51 @@ Describe 'Node' {
          Get-ReleaseDefinition -ProjectName $projectName | Should not be $Null
       }
 
+      It 'Should create azure serivce endpoint' {
+         Get-ServiceEndpoint -ProjectName $projectName | Should not be $Null
+      }
+
+      It 'Should create git repo endpoint' {
+         Get-GitRepository -ProjectName $projectName | Should not be $Null
+      }
+
       AfterAll {
          Get-Project | Where-Object {$_.Name -eq $projectName} | Remove-Project -Force
          Remove-Item -Recurse -Force -Path $projectName
       }
-   }   
+   }
+}
+
+Describe 'Node_Docker' {
+   $projectName = "project" + (New-Guid).Guid.SubString(0, 5)
+      
+   Context 'Full' {
+      
+      node $env:YO team node $projectName demonstrations PM_DonovanBrown " " " " " " Default docker false " " "tcp://my2016dockerhost.westus.cloudapp.azure.com:2376" ".\test" "https://index.docker.io/v1/" "unittest" "3000:3000" "unittests" " " $env:PAT *> .\nodeDocker.log
+      
+      It 'Should create project' {
+         Get-Project | Where-Object {$_.Name -eq $projectName} | Should not be $Null
+      }
+
+      It 'Should create build definition' {
+         Get-BuildDefinition -ProjectName $projectName | Should not be $Null
+      }
+
+      It 'Should create release definition' {
+         Get-ReleaseDefinition -ProjectName $projectName | Should not be $Null
+      }
+
+      It 'Should create both docker serivce endpoints' {
+         (Get-ServiceEndpoint -ProjectName $projectName).Count | Should be 2
+      }
+
+      It 'Should create git repo endpoint' {
+         Get-GitRepository -ProjectName $projectName | Should not be $Null
+      }
+
+      AfterAll {
+         #   Get-Project | Where-Object {$_.Name -eq $projectName} | Remove-Project -Force
+         #   Remove-Item -Recurse -Force -Path $projectName
+      }
+   }
 }
--- test/project.Tests.ps1
+++ test/project.Tests.ps1
@@ -2,7 +2,8 @@ Describe 'Project' {
    $projectName = "project" + (New-Guid).Guid.SubString(0, 5)
 
    Context 'Create' {
-
+      # The *> makes sure the ouput from yo does not throw off the build
+      # can cause false negatives 
       node $env:YO team:project $projectName demonstrations $env:PAT *> .\projectCreate.log
       
       It 'Should create project' {
