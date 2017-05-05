f4f187e28c8a262ff10b7976087d8a6152499263
Donovan Brown <dbrown@microsoft.com>
2017-05-05 10:08:23 -0500

    refactored app tests and added dockerpaas linux

--- test/appTests.js
+++ test/appTests.js
@@ -5,7 +5,55 @@ const assert = require(`yeoman-assert`);
 const util = require(`../generators/app/utility`);
 
 describe(`app:index`, () => {
+   it(`arguments using fake dependencies dockerpaas linux`, () => {
+      // Arrange
+      let deps = [
+         [helpers.createDummyGenerator(), `team:asp`],
+         [helpers.createDummyGenerator(), `team:git`],
+         [helpers.createDummyGenerator(), `team:azure`],
+         [helpers.createDummyGenerator(), `team:build`],
+         [helpers.createDummyGenerator(), `team:project`],
+         [helpers.createDummyGenerator(), `team:release`],
+         [helpers.createDummyGenerator(), `team:registry`]         
+      ];
+
+      let type = `asp`;
+      let name = `aspDemo`;
+      let tfs = `vsts`;
+      let azureSub = `AzureSub`;
+      let azureSubId = `AzureSubId`;
+      let tenantId = `TenantId`;
+      let servicePrincipalId = `servicePrincipalId`;
+      let queue = `Hosted Linux Preview`;
+      let target = `dockerpaas`;
+      let installDep = `false`;
+      let groupId = ``;
+      let dockerHost = ``;
+      let dockerCertPath = ``;
+      let dockerRegistry = `dockerRegistry`;
+      let dockerRegistryId = `dockerRegistryId`;
+      let dockerPorts = `dockerPorts`;
+      let dockerRegistryPassword = `dockerRegistryPassword`;
+      let servicePrincipalKey = `servicePrincipalKey`;
+      let pat = `token`;
+
+      // Act
+      return helpers.run(path.join(__dirname, `../generators/app/index`))
+         .withGenerators(deps)
+         .withArguments([type, name, tfs,
+            azureSub, azureSubId, tenantId, servicePrincipalId,
+            queue, target, installDep, groupId,
+            dockerHost, dockerCertPath,
+            dockerRegistry, dockerRegistryId, dockerPorts, dockerRegistryPassword,
+            servicePrincipalKey, pat
+         ])
+         .on(`error`, e => {
+            assert.fail(e);
+         });
+   });
+
    it(`arguments using fake dependencies paas`, () => {
+      // Arrange
       let deps = [
          [helpers.createDummyGenerator(), `team:asp`],
          [helpers.createDummyGenerator(), `team:git`],
@@ -15,20 +63,43 @@ describe(`app:index`, () => {
          [helpers.createDummyGenerator(), `team:release`]
       ];
 
+      let type = `asp`;
+      let name = `aspDemo`;
+      let tfs = `vsts`;
+      let azureSub = `AzureSub`;
+      let azureSubId = `AzureSubId`;
+      let tenantId = `TenantId`;
+      let servicePrincipalId = `servicePrincipalId`;
+      let queue = `default`;
+      let target = `paas`;
+      let installDep = `false`;
+      let groupId = ``;
+      let dockerHost = ``;
+      let dockerCertPath = ``;
+      let dockerRegistry = ``;
+      let dockerRegistryId = ``;
+      let dockerPorts = ``;
+      let dockerRegistryPassword = ``;
+      let servicePrincipalKey = `servicePrincipalKey`;
+      let pat = `token`;
+
+      // Act
       return helpers.run(path.join(__dirname, `../generators/app/index`))
          .withGenerators(deps)
-         .withArguments([`asp`, `aspDemo`, `vsts`,
-            `AzureSub`, `AzureSubId`, `TenantId`, `servicePrincipalId`,
-            `default`, `paas`,
-            `false`, ``,
-            ``, ``, ``, ``, ``, ``,
-            `servicePrincipalKey`, `token`])
+         .withArguments([type, name, tfs,
+            azureSub, azureSubId, tenantId, servicePrincipalId,
+            queue, target, installDep, groupId,
+            dockerHost, dockerCertPath,
+            dockerRegistry, dockerRegistryId, dockerPorts, dockerRegistryPassword,
+            servicePrincipalKey, pat
+         ])
          .on(`error`, e => {
             assert.fail(e);
          });
    });
 
    it(`arguments using fake dependencies aspFull paas`, () => {
+      // Arrange
       let deps = [
          [helpers.createDummyGenerator(), `team:aspFull`],
          [helpers.createDummyGenerator(), `team:git`],
@@ -38,14 +109,36 @@ describe(`app:index`, () => {
          [helpers.createDummyGenerator(), `team:release`]
       ];
 
+      let type = `aspFull`;
+      let name = `aspDemo`;
+      let tfs = `vsts`;
+      let azureSub = `AzureSub`;
+      let azureSubId = `AzureSubId`;
+      let tenantId = `TenantId`;
+      let servicePrincipalId = `servicePrincipalId`;
+      let queue = `default`;
+      let target = `paas`;
+      let installDep = `false`;
+      let groupId = ``;
+      let dockerHost = ``;
+      let dockerCertPath = ``;
+      let dockerRegistry = ``;
+      let dockerRegistryId = ``;
+      let dockerPorts = ``;
+      let dockerRegistryPassword = ``;
+      let servicePrincipalKey = `servicePrincipalKey`;
+      let pat = `token`;
+
+      // Act
       return helpers.run(path.join(__dirname, `../generators/app/index`))
          .withGenerators(deps)
-         .withArguments([`aspFull`, `aspDemo`, `vsts`,
-            `AzureSub`, `AzureSubId`, `TenantId`, `servicePrincipalId`,
-            `default`, `paas`,
-            `false`, ``,
-            ``, ``, ``, ``, ``, ``,
-            `servicePrincipalKey`, `token`])
+         .withArguments([type, name, tfs,
+            azureSub, azureSubId, tenantId, servicePrincipalId,
+            queue, target, installDep, groupId,
+            dockerHost, dockerCertPath,
+            dockerRegistry, dockerRegistryId, dockerPorts, dockerRegistryPassword,
+            servicePrincipalKey, pat
+         ])
          .on(`error`, e => {
             assert.fail(e);
          });
@@ -93,6 +186,7 @@ describe(`app:index`, () => {
    });
 
    it(`arguments using fake dependencies docker`, () => {
+      // Arrange
       let deps = [
          [helpers.createDummyGenerator(), `team:git`],
          [helpers.createDummyGenerator(), `team:node`],
@@ -103,13 +197,35 @@ describe(`app:index`, () => {
          [helpers.createDummyGenerator(), `team:registry`]
       ];
 
+      let type = `node`;
+      let name = `demo`;
+      let tfs = `vsts`;
+      let azureSub = ``;
+      let azureSubId = ``;
+      let tenantId = ``;
+      let servicePrincipalId = ``;
+      let queue = `default`;
+      let target = `docker`;
+      let installDep = `false`;
+      let groupId = ``;
+      let dockerHost = `dockerHost`;
+      let dockerCertPath = `dockerCertPath`;
+      let dockerRegistry = `dockerRegistry`;
+      let dockerRegistryId = `dockerRegistryId`;
+      let dockerPorts = `dockerPorts`;
+      let dockerRegistryPassword = `dockerRegistryPassword`;
+      let servicePrincipalKey = `servicePrincipalKey`;
+      let pat = `token`;
+
       return helpers.run(path.join(__dirname, `../generators/app/index`))
          .withGenerators(deps)
-         .withArguments([`node`, `demo`, `vsts`,
-            ``, ``, ``, ``, `default`, `docker`,
-            `false`, ``,
-            `dockerHost`, `dockerCertPath`, `dockerRegistryId`, `dockerRegistry`, `dockerPorts`, `dockerRegistryPassword`,
-            `servicePrincipalKey`, `token`])
+         .withArguments([type, name, tfs,
+            azureSub, azureSubId, tenantId, servicePrincipalId,
+            queue, target, installDep, groupId,
+            dockerHost, dockerCertPath,
+            dockerRegistry, dockerRegistryId, dockerPorts, dockerRegistryPassword,
+            servicePrincipalKey, pat
+         ])
          .on(`error`, e => {
             assert.fail(e);
          });
