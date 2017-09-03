83123738be2c0b17a30272292256cff6c0e81a66
Donovan Brown <dbrown@microsoft.com>
2017-09-03 11:16:44 +0200

    index app test

--- integration/test/appTests.js
+++ integration/test/appTests.js
@@ -1,10 +1,13 @@
 const path = require(`path`);
-const vsts = require(`./index.js`);
+const util = require(`./util`);
+const vsts = require(`./index`);
 const request = require('request');
 const env = require('node-env-file');
 const helpers = require(`yeoman-test`);
 const assert = require(`yeoman-assert`);
 const exec = require('child_process').exec;
+
+const userAgent = `yo team`;
 
 // Try to read values from .env. If that fails
 // simply use the environment vars on the machine.
@@ -13,12 +16,11 @@ env(__dirname + '/.env', {
    overwrite: true
 });
 
-describe.only(`build:index cmdLine`, () => {
+describe.only(`app:index cmdLine node paas`, () => {
    "use strict";
 
    var projectId;
    var expectedName;
-   var buildDefinitionId;
 
    // Arguments
    var applicationType = `node`;
@@ -43,41 +45,75 @@ describe.only(`build:index cmdLine`, () => {
 
    before(function (done) {
       // runs before all tests in this block
-      vsts.findProject(tfs, applicationName, pat, `yo Team`, (e, project) => {
-         assert.equal(project, undefined, `Precondition not meet: Project already exist`);
-         done(e);
+      vsts.findProject(tfs, applicationName, pat, userAgent, (e, project) => {
+
+         if (e) {
+            done(e);
+         }
+
+         if (project) {
+            projectId = project.id;
+            assert.equal(project, undefined, `Precondition not meet: Project already exist`);
+         }
+
+         // Run the command. The parts will be verified below.
+         let cmd = `yo team node ${applicationName} ${tfs} ${azureSub} "${azureSubId}" ` +
+            `"${tenantId}" "${servicePrincipalId}" ${queue} ${target} ${installDep} ` +
+            `"${groupId}" "${dockerHost}" "${dockerCertPath}" "${dockerRegistry}" ` +
+            `"${dockerRegistryId}" "${dockerPorts}" "${dockerRegistryPassword}" "${servicePrincipalKey}" ${pat}`;
+
+         util.log(`run command: ${cmd}`);
+
+         exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+               console.error(`exec error: ${error}`);
+               done(error);
+            }
+
+            util.log(`stdout: ${stdout}`);
+            util.log(`stderr: ${stderr}`);
+
+            // Find the project id
+            util.log(`find project: ${applicationName}`);
+
+            vsts.findProject(tfs, applicationName, pat, userAgent, (e, p) => {
+               if (e) {
+                  done(e);
+               }
+
+               projectId = p.id;
+
+               util.log(`project found: ${projectId}`);
+
+               done(e);
+            });
+         });
       });
    });
 
-   it(`node.js everything should be created`, (done) => {
+   it(`node.js build definition should be created`, (done) => {
       // Arrange
       expectedName = `nodeTest-CI`;
 
-      //let cmd = `yo team ${applicationType} ${applicationName} ${tfs} ${azureSub} '${azureSubId}' '${tenantId}' '${servicePrincipalId}' ${queue} ${target} ${installDep} '${groupId}' '${dockerHost}' '${dockerCertPath}' '${dockerRegistry}' "${dockerRegistryId}" "${dockerPorts}" "${dockerRegistryPassword}"'${servicePrincipalKey}' ${pat}`;
-      let cmd = `yo team node nodeTest2 demonstrations PM_DonovanBrown " " " " " " default paas false " " " " " " " " " " " " " " " " wkz4tdzpl37mu2pkysxfotpqb6lolly3w66klyjmwakdqupbh4za`;
+      util.log(`Find build ${expectedName}`);
 
-      exec(cmd, (error, stdout, stderr) => {
-         done(error);
+      vsts.findBuildDefinition(tfs, projectId, pat, expectedName, userAgent, (e, b) =>{
+         assert.ifError(e);
+         assert.ok(b, `build defintion not found`);
 
-         if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-         }
-         console.log(`stdout: ${stdout}`);
-         console.log(`stderr: ${stderr}`);
-      });
-   });
-
-   afterEach(function (done) {
-      // runs after each test in this block
-      vsts.deleteBuildDefinition(tfs, projectId, buildDefinitionId, pat, `yo team`, e => {
          done(e);
       });
    });
 
+   // runs after all tests in this block
    after(function (done) {
-      // runs after all tests in this block
-      vsts.deleteProject(tfs, projectId, pat, `yo team`, (e) => {
+      util.log(`delete project: ${projectId}`);
+
+      vsts.deleteProject(tfs, projectId, pat, userAgent, (e) => {
+         util.log(`delete folder: ${applicationName}`);
+
+         util.rmdir(applicationName);
+
          done(e);
       });
    });
---
+++ integration/test/util.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const env = require('node-env-file');
+
+// Try to read values from .env. If that fails
+// simply use the environment vars on the machine.
+env(__dirname + '/.env', {
+   raise: false,
+   overwrite: true
+});
+
+var logging = process.env.LOG || `off`;
+
+var logMessage = function (msg) {
+   if (logging === `on`) {
+      console.log(msg);
+   }
+};
+
+var deleteFolderRecursive = function (path) {
+   if (fs.existsSync(path)) {
+      fs.readdirSync(path).forEach(function (file, index) {
+         var curPath = path + "/" + file;
+
+         if (fs.lstatSync(curPath).isDirectory()) { // recurse
+            deleteFolderRecursive(curPath);
+         } else { // delete file
+            fs.unlinkSync(curPath);
+         }
+      });
+
+      fs.rmdirSync(path);
+   }
+};
+
+module.exports = {
+
+   // Exports the portions of the file we want to share with files that require
+   // it.
+
+   rmdir: deleteFolderRecursive,
+   log: logMessage
+};
