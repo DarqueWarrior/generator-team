d1d1cfbe916dc4e70697898d464f22f43c2ffea2
yo team <yo team>
2017-09-14 16:22:30 -0500

    Fixed path for templates in Java Docker Build.

--- integration/test/dockerACIDefaultAgentTests.js
+++ integration/test/dockerACIDefaultAgentTests.js
@@ -2,21 +2,23 @@ const docker = require('./docker');
 
 describe(`Azure Container Instances (Linux) using Default queue`, function () {
    "use strict";
-   var iterations = [{
-      appType: `node`,
-      appName: `nodeACITest`,
-      target: `acilinux`,
-      queue: `Default`,
-      groupId: ` `,
-      title: `Home Page - My Express Application`
-   }, {
-      appType: `asp`,
-      appName: `aspACITest`,
-      target: `acilinux`,
-      queue: `Default`,
-      groupId: ` `,
-      title: `Home Page - My .NET Core Application`
-   }, {
+   var iterations = [
+   //    {
+   //    appType: `node`,
+   //    appName: `nodeACITest`,
+   //    target: `acilinux`,
+   //    queue: `Default`,
+   //    groupId: ` `,
+   //    title: `Home Page - My Express Application`
+   // }, {
+   //    appType: `asp`,
+   //    appName: `aspACITest`,
+   //    target: `acilinux`,
+   //    queue: `Default`,
+   //    groupId: ` `,
+   //    title: `Home Page - My .NET Core Application`
+   // },
+    {
       appType: `java`,
       appName: `javaACITest`,
       target: `acilinux`,
