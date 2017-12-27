814c6e2598a46d6e964a9086dde8dab4542796c1
Donovan Brown <dbrown@microsoft.com>
2017-12-27 11:21:29 -0600

    Clean up

--- integration/spnCleanUp.ps1
+++
@@ -1,7 +0,0 @@
-Connect-AzureAD
-Get-AzureADApplication -SearchString demonstrations
-Get-AzureADApplication -SearchString demonstrations | Remove-AzureADApplication
-Get-AzureADDeletedApplication -SearchString demonstrations -All $true | Select-Object -ExpandProperty objectid
-Get-AzureADDeletedApplication -SearchString demonstrations -All $true | Select-Object -ExpandProperty objectid | Remove-AzureADMSDeletedDirectoryObject -Verbose
-
-#Get-AzureADDeletedApplication -All $true | where {$_.Displayname -like 'VisualStudioSPN*'} | select -ExpandProperty objectid | ForEach-Object { Remove-AzureADMSDeletedDirectoryObject -Id $_ -ErrorAction Continue }
