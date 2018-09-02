e073ab724ffec2a2fb83c11376a121747282d745
yo team <yo team>
2018-09-02 13:59:22 -0500

    Added client side application insights. Fixed ARM template for nodejs

--- generators/node/templates/src/routes/homeController.js
+++ generators/node/templates/src/routes/homeController.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var utils = require('../utility');
 
 // application insights
-let client = utils.initAppInsights()
+let client = utils.initAppInsights();
+let key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
    client.trackNodeHttpRequest({request: req, response: res});
    client.trackEvent({name: "Get Index", properties: {customProperty: "route is /"}});
-   res.render('index', { title: 'Home Page' });
+   res.render('index', { title: 'Home Page', instrumentationKey: key });
 });
 
 router.get('/contact', function (req, res, next) {
    client.trackEvent({name: "Get Contact", properties: {customProperty: "route is /contact"}});
-   res.render('contact', { title: 'Contact', message: 'Your contact page.' });
+   res.render('contact', { title: 'Contact', message: 'Your contact page.', instrumentationKey: key });
 });
 
 router.get('/about', function (req, res, next) {
    client.trackEvent({name: "Get About", properties: {customProperty: "route is /about"}});
-   res.render('about', { title: 'About', message: 'Your application description page.' });
+   res.render('about', { title: 'About', message: 'Your application description page.', instrumentationKey: key });
 });
 
 module.exports = router;
