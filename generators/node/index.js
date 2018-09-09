51aa802f0bec7264fe6744bf60db7b259b7408c4
yo team <yo team>
2018-09-09 10:38:55 -0500

    Added ping test to node.js Docker ARM Template.

--- generators/node/index.js
+++ generators/node/index.js
@@ -91,7 +91,7 @@ module.exports = class extends Generator {
       this.fs.copy(`${src}/acilinux_arm.json`, `${root}/acilinux.json`);
       this.fs.copyTpl(`${src}/acilinux_arm.parameters.json`, `${root}/acilinux.parameters.json`, tokens);
 
-      this.fs.copy(`${src}/docker_arm.json`, `${root}/docker.json`);
+      this.fs.copyTpl(`${src}/docker_arm.json`, `${root}/docker.json`, tokens);
       this.fs.copy(`${src}/docker_arm.parameters.json`, `${root}/docker.parameters.json`);
    }
 
