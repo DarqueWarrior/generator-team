d66f14b3da551a39dc2630ed100d748ae64b83bb
Donovan <dbrown@microsoft.com>
2018-10-09 12:38:27 -0500

    Node with Yarn is working on Azure DevOps

--- generators/node/templates/src/utility.js
+++ generators/node/templates/src/utility.js
@@ -3,7 +3,9 @@ var appInsights = require("applicationinsights");
 // Used to wire up Application Insights
 // Moving the code here makes it easier to mock.
 function initAppInsights() {
-   appInsights.setup().start();
+   // If the user does not set the key just skip setup
+   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+      appInsights.setup().start();
 
    return appInsights.defaultClient;
 }
